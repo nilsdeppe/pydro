b1605e491540f9d44aaab529f4eaa77456fb37e4
Nils Deppe <nd357@cornell.edu>
2019-10-23 16:55:46 -0400

    Add RK6SSP

--- TimeStepper.py
+++ TimeStepper.py
@@ -81,6 +81,58 @@ class Rk4Ssp:
         self._time += dt
 
 
+class Rk6Ssp:
+    """
+    A sixth-order SSP Runge-Kutta method from
+    Strong Stability Preserving High Order Time Discretization Methods
+    by Gottlieb, Shu, and Tadmor
+    """
+    _compute_time_deriv = None
+    _evolved_vars = None
+    _time = None
+    _cfl_coefficient = 1.0
+
+    def __init__(self, time_deriv, initial_state, initial_time):
+        self._compute_time_deriv = time_deriv
+        self._evolved_vars = np.copy(initial_state)
+        self._time = initial_time
+
+    def get_evolved_vars(self):
+        return self._evolved_vars
+
+    def get_time(self):
+        return self._time
+
+    def get_cfl_coefficient(self):
+        return self._cfl_coefficient
+
+    def take_step(self, dt):
+        k0 = self._compute_time_deriv(self._evolved_vars, self._time)
+
+        v1 = self._evolved_vars + dt * k0
+        k1 = self._compute_time_deriv(v1, self._time)
+
+        v2 = v1 + dt * k1
+        k2 = self._compute_time_deriv(v2, self._time)
+
+        v3 = v2 + dt * k2
+        k3 = self._compute_time_deriv(v3, self._time)
+
+        v4 = v3 + dt * k3
+        k4 = self._compute_time_deriv(v4, self._time)
+
+        v5 = v4 + dt * k4
+        k5 = self._compute_time_deriv(v5, self._time)
+
+        self._evolved_vars = (0.3680555555555556 * self._evolved_vars +
+                              0.36666666666666664 * v1 + 0.1875 * v2 +
+                              0.05555555555555555 * v3 +
+                              0.020833333333333332 * v4 +
+                              0.001388888888888889 * (v5 + dt * k5))
+
+        self._time += dt
+
+
 def _adams_bashforth1(history, evolved_vars, dvars_dt, dt):
     for j in range(len(dvars_dt)):
         evolved_vars[j] += dt * history[0][j]
