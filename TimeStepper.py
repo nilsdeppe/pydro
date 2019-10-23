560390f07f7350a7001ed346a647c4a13c38be33
Nils Deppe <nd357@cornell.edu>
2019-10-23 16:46:44 -0400

    Add RK4SSP

--- TimeStepper.py
+++ TimeStepper.py
@@ -27,14 +27,57 @@ class Rk3Ssp:
         return self._cfl_coefficient
 
     def take_step(self, dt):
-        dt_vars = self._compute_time_deriv(self._evolved_vars,
-                                           self._time)
+        dt_vars = self._compute_time_deriv(self._evolved_vars, self._time)
         v1 = self._evolved_vars + dt * dt_vars
         dt_vars = dt_vars = self._compute_time_deriv(v1, self._time + dt)
         v2 = 0.25 * (3.0 * self._evolved_vars + v1 + dt * dt_vars)
         dt_vars = self._compute_time_deriv(v2, self._time + 0.5 * dt)
         self._evolved_vars = 1.0 / 3.0 * (self._evolved_vars + 2.0 * v2 +
                                           2.0 * dt * dt_vars)
+        self._time += dt
+
+
+class Rk4Ssp:
+    """
+    A fourth-order SSP Runge-Kutta method from
+    Strong Stability Preserving High Order Time Discretization Methods
+    by Gottlieb, Shu, and Tadmor
+    """
+    _compute_time_deriv = None
+    _evolved_vars = None
+    _time = None
+    _cfl_coefficient = 1.0
+
+    def __init__(self, time_deriv, initial_state, initial_time):
+        self._compute_time_deriv = time_deriv
+        self._evolved_vars = np.copy(initial_state)
+        self._time = initial_time
+
+    def get_evolved_vars(self):
+        return self._evolved_vars
+
+    def get_time(self):
+        return self._time
+
+    def get_cfl_coefficient(self):
+        return self._cfl_coefficient
+
+    def take_step(self, dt):
+        k0 = self._compute_time_deriv(self._evolved_vars, self._time)
+
+        v1 = self._evolved_vars + dt * k0
+        k1 = self._compute_time_deriv(v1, self._time)
+
+        v2 = v1 + dt * k1
+        k2 = self._compute_time_deriv(v2, self._time)
+
+        v3 = v2 + dt * k2
+        k3 = self._compute_time_deriv(v3, self._time)
+
+        self._evolved_vars = (0.375 * self._evolved_vars +
+                              0.333333333333333333 * v1 + 0.25 * v2 +
+                              0.041666666666666664 * (v3 + dt * k3))
+
         self._time += dt
 
 
