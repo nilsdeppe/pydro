15b06bbd833ec50fd4333e56c74541f6fca4ebb9
Nils Deppe <nd357@cornell.edu>
2019-10-24 10:56:49 -0400

    Add more FD orders
    Add MD orders 2-10 and MND orders 2-10 to make comparison between different
    schemes easier.

--- Derivative.py
+++ Derivative.py
@@ -15,8 +15,14 @@ except:
 class Scheme(enum.Enum):
     MD = enum.auto()
     MD4 = enum.auto()
+    MD6 = enum.auto()
+    MD8 = enum.auto()
+    MD10 = enum.auto()
+    MDV = enum.auto()
     MND4 = enum.auto()
     MND6 = enum.auto()
+    MND8 = enum.auto()
+    MND10 = enum.auto()
     MNDV = enum.auto()
 
 
@@ -25,8 +31,9 @@ def _md_impl_(dx, numerical_flux, center_flux, order_used):
     Implements the 2nd order finite difference using numerical fluxes
     """
     result = []
+    inv_dx = 1.0 / dx
     for i in range(len(numerical_flux)):
-        result.append(1.0 / dx *
+        result.append(inv_dx *
                       (numerical_flux[i][1:] - numerical_flux[i][:-1]))
     return result
 
@@ -36,13 +43,135 @@ def _md4_impl_(dx, numerical_flux, center_flux, order_used):
     Implements the 4th order finite difference using numerical fluxes
     """
     result = []
+    inv_dx = 1.0 / dx
     for j in range(len(numerical_flux)):
         temp = np.zeros(len(numerical_flux[j]) - 1)
         for i in range(1, len(numerical_flux[j]) - 2):
-            temp[i] = 9.0 / 8.0 * (numerical_flux[j][i + 1] -
-                                   numerical_flux[j][i]) / dx - 1.0 / 24.0 * (
+            temp[i] = 1.125 * (numerical_flux[j][i + 1] - numerical_flux[j][i]
+                               ) * inv_dx - 0.041666666666666664 * (
+                                   numerical_flux[j][i + 2] -
+                                   numerical_flux[j][i - 1]) * inv_dx
+        result.append(temp)
+    return result
+
+
+def _md6_impl_(dx, numerical_flux, center_flux, order_used):
+    """
+    Implements the 6th order finite difference using numerical fluxes
+    """
+    result = []
+    inv_dx = 1.0 / dx
+    for j in range(len(numerical_flux)):
+        temp = np.zeros(len(numerical_flux[j]) - 1)
+        for i in range(2, len(numerical_flux[j]) - 3):
+            temp[i] = (
+                1.171875 * (numerical_flux[j][i + 1] - numerical_flux[j][i]) -
+                0.06510416666666667 *
+                (numerical_flux[j][i + 2] - numerical_flux[j][i - 1]) +
+                0.0046875 *
+                (numerical_flux[j][i + 3] - numerical_flux[j][i - 2])) * inv_dx
+        result.append(temp)
+    return result
+
+
+def _md8_impl_(dx, numerical_flux, center_flux, order_used):
+    """
+    Implements the 8th order finite difference using numerical fluxes
+    """
+    result = []
+    inv_dx = 1.0 / dx
+    for j in range(len(numerical_flux)):
+        temp = np.zeros(len(numerical_flux[j]) - 1)
+        for i in range(3, len(numerical_flux[j]) - 4):
+            temp[i] = (
+                1.1962890625 *
+                (numerical_flux[j][i + 1] - numerical_flux[j][i]) -
+                0.07975260416666667 *
+                (numerical_flux[j][i + 2] - numerical_flux[j][i - 1]) +
+                0.0095703125 *
+                (numerical_flux[j][i + 3] - numerical_flux[j][i - 2]) -
+                0.0006975446428571429 *
+                (numerical_flux[j][i + 4] - numerical_flux[j][i - 3])) * inv_dx
+        result.append(temp)
+    return result
+
+
+def _md10_impl_(dx, numerical_flux, center_flux, order_used):
+    """
+    Implements the 10th order finite difference using numerical fluxes
+    """
+    result = []
+    inv_dx = 1.0 / dx
+    for j in range(len(numerical_flux)):
+        temp = np.zeros(len(numerical_flux[j]) - 1)
+        for i in range(4, len(numerical_flux[j]) - 5):
+            temp[i] = (
+                1.21124267578125 *
+                (numerical_flux[j][i + 1] - numerical_flux[j][i]) -
+                0.0897216796875 *
+                (numerical_flux[j][i + 2] - numerical_flux[j][i - 1]) +
+                0.0138427734375 *
+                (numerical_flux[j][i + 3] - numerical_flux[j][i - 2]) -
+                0.0017656598772321428 *
+                (numerical_flux[j][i + 4] - numerical_flux[j][i - 3]) +
+                0.00011867947048611111 *
+                (numerical_flux[j][i + 5] - numerical_flux[j][i - 4])) * inv_dx
+        result.append(temp)
+    return result
+
+
+def _mdv_impl_(dx, numerical_flux, center_flux, order_used):
+    result = []
+    inv_dx = 1.0 / dx
+    for j in range(len(numerical_flux)):
+        temp = np.zeros(len(numerical_flux[j]) - 1)
+        for i in range(4, len(center_flux[j]) - 5):
+            if order_used[i] > 8:
+                # Tenth order
+                temp[i] = (
+                    1.21124267578125 *
+                    (numerical_flux[j][i + 1] - numerical_flux[j][i]) -
+                    0.0897216796875 *
+                    (numerical_flux[j][i + 2] - numerical_flux[j][i - 1]) +
+                    0.0138427734375 *
+                    (numerical_flux[j][i + 3] - numerical_flux[j][i - 2]) -
+                    0.0017656598772321428 *
+                    (numerical_flux[j][i + 4] - numerical_flux[j][i - 3]) +
+                    0.00011867947048611111 *
+                    (numerical_flux[j][i + 5] - numerical_flux[j][i - 4])
+                ) * inv_dx
+            elif order_used[i] > 6:
+                # Eighth order
+                temp[i] = (
+                    1.1962890625 *
+                    (numerical_flux[j][i + 1] - numerical_flux[j][i]) -
+                    0.07975260416666667 *
+                    (numerical_flux[j][i + 2] - numerical_flux[j][i - 1]) +
+                    0.0095703125 *
+                    (numerical_flux[j][i + 3] - numerical_flux[j][i - 2]) -
+                    0.0006975446428571429 *
+                    (numerical_flux[j][i + 4] - numerical_flux[j][i - 3])
+                ) * inv_dx
+            elif order_used[i] > 4:
+                # Sixth order
+                temp[i] = (
+                    1.171875 *
+                    (numerical_flux[j][i + 1] - numerical_flux[j][i]) -
+                    0.06510416666666667 *
+                    (numerical_flux[j][i + 2] - numerical_flux[j][i - 1]) +
+                    0.0046875 *
+                    (numerical_flux[j][i + 3] - numerical_flux[j][i - 2])
+                ) * inv_dx
+            elif order_used[i] > 2:
+                # Fourth order
+                temp[i] = 1.125 * (numerical_flux[j][i + 1] - numerical_flux[j]
+                                   [i]) * inv_dx - 0.041666666666666664 * (
                                        numerical_flux[j][i + 2] -
-                                       numerical_flux[j][i - 1]) / dx
+                                       numerical_flux[j][i - 1]) * inv_dx
+            else:
+                f_p = numerical_flux[j][i + 1]
+                f_m = numerical_flux[j][i]
+                temp[i] = (f_p - f_m) * inv_dx
         result.append(temp)
     return result
 
@@ -55,6 +184,7 @@ def _mnd4_impl_(dx, numerical_flux, center_flux, order_used):
     if center_flux is None:
         raise TypeError("Center flux must not be None")
     result = []
+    inv_dx = 1.0 / dx
     for j in range(len(numerical_flux)):
         temp = np.zeros(len(numerical_flux[j]) - 1)
         for i in range(1, len(center_flux[j]) - 1):
@@ -62,7 +192,7 @@ def _mnd4_impl_(dx, numerical_flux, center_flux, order_used):
                 i + 1] - 1.0 / 6.0 * center_flux[j][i + 1]
             f_m = 4.0 / 3.0 * numerical_flux[j][i] - 1.0 / 6.0 * center_flux[
                 j][i - 1]
-            temp[i] = (f_p - f_m) / dx
+            temp[i] = (f_p - f_m) * inv_dx
         result.append(temp)
     return result
 
@@ -75,6 +205,7 @@ def _mnd6_impl_(dx, numerical_flux, center_flux, order_used):
     if center_flux is None:
         raise TypeError("Center flux must not be None")
     result = []
+    inv_dx = 1.0 / dx
     for j in range(len(numerical_flux)):
         temp = np.zeros(len(numerical_flux[j]) - 1)
         for i in range(1, len(center_flux[j]) - 2):
@@ -83,7 +214,59 @@ def _mnd6_impl_(dx, numerical_flux, center_flux, order_used):
                     i + 1] + 1.0 / 30.0 * numerical_flux[j][i + 2]
             f_m = 3.0 / 2.0 * numerical_flux[j][i] - 3.0 / 10.0 * center_flux[
                 j][i - 1] + 1.0 / 30.0 * numerical_flux[j][i - 1]
-            temp[i] = (f_p - f_m) / dx
+            temp[i] = (f_p - f_m) * inv_dx
+        result.append(temp)
+    return result
+
+
+def _mnd8_impl_(dx, numerical_flux, center_flux, order_used):
+    """
+    Implements the 8th order midpoint-and-node-to-node differencing of
+    http://dx.doi.org/10.1016/j.compfluid.2012.09.001
+    """
+    if center_flux is None:
+        raise TypeError("Center flux must not be None")
+    result = []
+    inv_dx = 1.0 / dx
+    for j in range(len(numerical_flux)):
+        temp = np.zeros(len(numerical_flux[j]) - 1)
+        for i in range(1, len(center_flux[j]) - 2):
+            f_p = 1.6 * numerical_flux[j][i + 1] - 0.4 * center_flux[j][
+                i + 1] + 0.0761904761904762 * numerical_flux[j][
+                    i + 2] - 0.007142857142857143 * center_flux[j][i + 2]
+            f_m = 1.6 * numerical_flux[j][i] - 0.4 * center_flux[j][
+                i - 1] + 0.0761904761904762 * numerical_flux[j][
+                    i - 1] - 0.007142857142857143 * center_flux[j][i - 2]
+            temp[i] = (f_p - f_m) * inv_dx
+        result.append(temp)
+    return result
+
+
+def _mnd10_impl_(dx, numerical_flux, center_flux, order_used):
+    """
+    Implements the 10th order midpoint-and-node-to-node differencing of
+    http://dx.doi.org/10.1016/j.compfluid.2012.09.001
+    """
+    if center_flux is None:
+        raise TypeError("Center flux must not be None")
+    result = []
+    inv_dx = 1.0 / dx
+    for j in range(len(numerical_flux)):
+        temp = np.zeros(len(numerical_flux[j]) - 1)
+        for i in range(1, len(center_flux[j]) - 2):
+            f_p = 1.6666666666666667 * numerical_flux[j][
+                i + 1] - 0.47619047619047616 * center_flux[j][
+                    i + 1] + 0.11904761904761904 * numerical_flux[j][
+                        i + 2] - 0.01984126984126984 * center_flux[j][
+                            i + 2] + 0.0015873015873015873 * numerical_flux[j][
+                                i + 3]
+            f_m = 1.6666666666666667 * numerical_flux[j][
+                i] - 0.47619047619047616 * center_flux[j][
+                    i - 1] + 0.11904761904761904 * numerical_flux[j][
+                        i - 1] - 0.01984126984126984 * center_flux[j][
+                            i - 2] + 0.0015873015873015873 * numerical_flux[j][
+                                i - 2]
+            temp[i] = (f_p - f_m) * inv_dx
         result.append(temp)
     return result
 
@@ -143,22 +326,40 @@ def _mndv_impl_(dx, numerical_flux, center_flux, order_used):
 if use_numba:
     _md_impl = nb.jit(nopython=True)(_md_impl_)
     _md4_impl = nb.jit(nopython=True)(_md4_impl_)
+    _md6_impl = nb.jit(nopython=True)(_md6_impl_)
+    _md8_impl = nb.jit(nopython=True)(_md8_impl_)
+    _md10_impl = nb.jit(nopython=True)(_md10_impl_)
+    _mdv_impl = nb.jit(nopython=True)(_mdv_impl_)
     _mnd4_impl = nb.jit(nopython=True)(_mnd4_impl_)
     _mnd6_impl = nb.jit(nopython=True)(_mnd6_impl_)
+    _mnd8_impl = nb.jit(nopython=True)(_mnd8_impl_)
+    _mnd10_impl = nb.jit(nopython=True)(_mnd10_impl_)
     _mndv_impl = nb.jit(nopython=True)(_mndv_impl_)
 else:
     print("Please install Numba for better performance.")
     _md_impl = _md_impl_
     _md4_impl = _md4_impl_
+    _md6_impl = _md6_impl_
+    _md8_impl = _md8_impl_
+    _md10_impl = _md10_impl_
+    _mdv_impl = _mdv_impl_
     _mnd4_impl = _mnd4_impl_
     _mnd6_impl = _mnd6_impl_
+    _mnd8_impl = _mnd8_impl_
+    _mnd10_impl = _mnd10_impl_
     _mndv_impl = _mndv_impl_
 
 _deriv_dispatch = {
     Scheme.MD: _md_impl,
     Scheme.MD4: _md4_impl,
+    Scheme.MD6: _md6_impl,
+    Scheme.MD8: _md8_impl,
+    Scheme.MD10: _md10_impl,
+    Scheme.MDV: _mdv_impl,
     Scheme.MND4: _mnd4_impl,
     Scheme.MND6: _mnd6_impl,
+    Scheme.MND8: _mnd8_impl,
+    Scheme.MND10: _mnd10_impl,
     Scheme.MNDV: _mndv_impl,
 }
 
